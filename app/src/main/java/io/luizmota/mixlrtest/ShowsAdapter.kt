58a0dba92bd66781a90093fc9ea4dea452c4878f
Luiz Mota <code@luizmota.io>
2020-05-27 16:24:31 +0100

    Add date parsing and sort shows by start time

--- app/src/main/java/io/luizmota/mixlrtest/ShowsAdapter.kt
+++ app/src/main/java/io/luizmota/mixlrtest/ShowsAdapter.kt
@@ -3,10 +3,12 @@ package io.luizmota.mixlrtest
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.bumptech.glide.RequestManager
 import kotlinx.android.synthetic.main.show_list_item.view.*
+import java.text.SimpleDateFormat
 
 internal class ShowsAdapter(val requestManager: RequestManager) : Adapter<ShowItemViewHolder>() {
     private var shows: List<Show> = emptyList()
@@ -40,16 +42,35 @@ internal class ShowItemViewHolder(itemView: View, val requestManager: RequestMan
         }
         itemView.show_title.text = show.title
         itemView.show_host.text = show.host
-        itemView.show_start_time.text = itemView.resources.getString(R.string.show_begins_at, show.startTime)
-        itemView.show_end_time.text = itemView.resources.getString(R.string.show_ends_at, show.endTime)
+
+        val dateTimeInstance = SimpleDateFormat.getDateTimeInstance()
+        itemView.show_start_time.text = itemView.resources.getString(
+            R.string.show_begins_at,
+            dateTimeInstance.format(show.startTime)
+        )
+        itemView.show_end_time.text = itemView.resources.getString(
+            R.string.show_ends_at,
+            dateTimeInstance.format(show.endTime)
+        )
 
         when {
             show.isOnAir -> {
-                itemView.show_media_toggle.setImageDrawable(itemView.resources.getDrawable(R.drawable.ic_play_arrow_black_24dp))
+
+                itemView.show_media_toggle.setImageDrawable(
+                    ResourcesCompat.getDrawable(
+                        itemView.resources,
+                        R.drawable.ic_play_arrow_black_24dp,
+                        null
+                    ))
                 itemView.show_media_toggle.visibility = View.VISIBLE
             }
             show.isLive -> {
-                itemView.show_media_toggle.setImageDrawable(itemView.resources.getDrawable(R.drawable.ic_stop_black_24dp))
+                itemView.show_media_toggle.setImageDrawable(
+                    ResourcesCompat.getDrawable(
+                        itemView.resources,
+                        R.drawable.ic_stop_black_24dp,
+                        null
+                    ))
                 itemView.show_media_toggle.visibility = View.VISIBLE
             }
             else -> itemView.show_media_toggle.visibility = View.INVISIBLE
